c1d4c8d6dfe1955c5d5483a88a8110991cd92f6c
dianeegranger <59130351+dianeegranger@users.noreply.github.com>
2023-08-27 23:50:42 -0400

    Submitted React file App.jsx
    Displays "Hello, World! 🌺🌈" message on the webpage.

---
+++ App.jsx
@@ -0,0 +1,15 @@
+/*
+*   React
+*   Display a "Hello, World! 🌺🌈" 
+*   message on webpage
+*
+*   Author:  Diane E. Granger
+*            dianeegranger@gmail.com
+**************************************/
+const App = () => {
+  
+  return <h2>Hello, World! 🌺🌈</h2>
+    
+};
+    
+export default App;
